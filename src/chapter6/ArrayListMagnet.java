9e25994714b1c8f55587fd42fc04e0dac86533da
dmitriy_shirmanov <shirmanovdmitriy@gmail.com>
2025-12-11 21:32:25 +0700

    Задание с магнитиками глава 6

---
+++ src/chapter6/ArrayListMagnet.java
@@ -0,0 +1,37 @@
+package chapter6;
+
+import java.util.*;
+
+public class ArrayListMagnet {
+    public static void printAl(ArrayList<String> a1) {
+        for (String element : a1) {
+            System.out.print(element + "  ");
+        }
+
+        System.out.println("  ");
+    }
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<>();
+        a.add(0, "ноль");
+        a.add(1,"один");
+        a.add(2, "два");
+        a.add(3, "три");
+        printAl(a);
+
+        if (a.contains("три")) {
+            a.add("четыре");
+        }
+        a.remove(2);
+        printAl(a);
+
+        if (a.indexOf("четыри") != 4) {
+            a.add(4, "4.2");
+        }
+        printAl(a);
+
+        if(a.contains("два")) {
+            a.add("2.2");
+        }
+        printAl(a);
+    }
+}
